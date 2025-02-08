4e97c294a442a330b3ece61ff8276a969b42766c
Arctik16 <artemmalikov2004@gmail.com>
2025-02-08 16:20:02 +0300

    Add new files hw05

---
+++ hw05/build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/hw05/tests
+# Build directory: D:/hw05/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hw05_tests "hw05_test")
+set_tests_properties(hw05_tests PROPERTIES  _BACKTRACE_TRIPLES "D:/hw05/tests/CMakeLists.txt;11;add_test;D:/hw05/tests/CMakeLists.txt;0;")
